a6fc1e22d77eb64f7e96cbe22ed5ded56599b000
Leandro Rodrigues <102068625+leandrotecnologia@users.noreply.github.com>
2022-10-19 20:08:35 -0300

    Add files via upload

---
+++ leandrorodriguesmonteiro.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main (){
+	
+printf("Materia: Algoritimo e Progamacao \n");
+printf("Professora: Joyce Siqueira \n");
+printf("Nome: Leandro rodrigues monteiro \n");
+printf("Curso: Analise e desenvolvimento de sistemas \n");
+printf("GitHub: https://github.com/leandrotecnologia/leandrorodriguesmonteiro.c.git \n");
+printf("Software: DevC++");
+}
